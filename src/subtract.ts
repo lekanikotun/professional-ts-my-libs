2eb5a529e463924c1dddb997e0b24b369ea90ff4
Lekan Ikotun <lekanikotun@gmail.com>
2020-10-13 16:48:38 -0400

    typescript repo

---
+++ src/subtract.ts
@@ -0,0 +1,11 @@
+/**
+ * Calc the difference between 2 numbers
+ * 
+ * @param a - first number
+ * @param b - second number
+ * 
+ * @beta
+ */
+export function subtract(a: number, b: number): number {
+    return a - b;
+}
